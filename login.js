d203709249e48a76e07eed094c2898ceddbdc292
Wesley <wesleyqueirozdeveloper@gmail.com>
2025-02-25 17:36:50 -0300

    Enhance login.js with additional functions for user authentication and update empresa.txt with new entries

--- login.js
+++ login.js
@@ -1,1 +1,27 @@
 // trabalhando no login
+// 1. pegar os dados do form
+// 2. validar os dados
+function teste(){
+    console.log('teste')
+}
+function login(){
+    console.log('login')
+}
+function logout(){
+    console.log('logout')
+}
+function register(){
+    console.log('register')
+}
+function forgot(){
+    console.log('forgot')
+}
+function reset(){
+    console.log('reset')
+}
+function change(){
+    console.log('change')
+}
+function profile(){
+    console.log('profile')
+}
